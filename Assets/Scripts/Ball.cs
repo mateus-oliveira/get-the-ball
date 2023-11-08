41ea92f2a04c49cdc0390c3021060dd72ad70bd2
Mateus Oliveira <devmateusalves@gmail.com>
2023-11-08 13:07:25 -0300

    level4 and medium rope

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Goal")) {
+            rb.bodyType = RigidbodyType2D.Static;
+            gameObject.transform.position = other.transform.position;
+        }
     }
 }
